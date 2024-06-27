c34cac0ba06a47f310651dfd624511c8b54f9578
Gene Navera <gnavera@gmail.com>
2024-06-27 14:24:49 +0800

    fixed backend environments

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+wait-for-it 10.2.0.10:3306
+
 composer install
 
 if ! [ -f .env ]; then
     echo "ENV file does not exist, generating.."
     cp .env.example .env
+    touch storage/logs/laravel.log
 
     php artisan key:generate
     php artisan migrate --seed
@@ -11,6 +14,4 @@ else
     php artisan migrate --force
 fi
 
-php artisan optimize
-php artisan route:clear
 php artisan serve --host=0.0.0.0
